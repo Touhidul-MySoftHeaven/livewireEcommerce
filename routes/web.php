9a2e3acd4fc725cda9513c13367d3ce3c23128dd
Touhidul-MySoftHeaven <touhidul.developer.2022@gmail.com>
2022-12-15 10:04:43 +0600

    first code

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\AboutUsComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactUsComponent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class);
+Route::get('/about-us', AboutUsComponent::class);
+Route::get('/shop', ShopComponent::class);
+Route::get('/contact-us', ContactUsComponent::class);
+Route::get('/cart', CartComponent::class);
+Route::get('/checkout',CheckoutComponent::class);
